67359f014815674aeeb0bd36d14b02e107f2ccd7
Akhil5131 <113959109+Akhil5131@users.noreply.github.com>
2023-12-20 01:58:53 -0500

    intial commit

---
+++ springBootJpaNewProject/src/main/java/com/example/demo/service/EmployeeServiceimpl.java
@@ -0,0 +1,45 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import com.example.demo.entity.Employee;
+
+public class EmployeeServiceimpl implements EmployeeService {
+
+	@Override
+	public Employee saveEmployee(Employee employee) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Employee updateEmployee(Employee employee) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteEmployee(Employee employee) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Employee findById(long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Employee findAll(Employee employee) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Employee> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
