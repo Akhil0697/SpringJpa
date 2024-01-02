29d8688408191af0bf1a4aee372b54f7852993e0
Akhil5131 <113959109+Akhil5131@users.noreply.github.com>
2024-01-02 14:28:19 -0500

    entity changes

--- springBootJpaNewProject/src/main/java/com/example/demo/controller/HomeController.java
+++ springBootJpaNewProject/src/main/java/com/example/demo/controller/HomeController.java
@@ -1,6 +1,8 @@
 package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,12 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.entity.Employee;
 import com.example.demo.service.EmployeeService;
 
-@RestController
+@Controller
 public class HomeController {
 	@Autowired
 	private EmployeeService employeeService;
 	@GetMapping("/")
-	public String insertEmployee()
+	public String insertEmployee(Model model)
 	{
 		Employee employee = new Employee();
 		employee.setName("Kishore");
@@ -21,8 +23,11 @@ public class HomeController {
 		employee.setDepartment("Accounts");
 		employee.setExp(22);
 		
-	Employee emp = 	employeeService.saveEmployee(employee);
-		return "Employee "+emp.getName()+" with id "+emp.getId()+" is saved successfully";
+	    Employee emp = 	employeeService.saveEmployee(employee);
+		String msg = "Employee "+emp.getName()+" with id "+emp.getId()+" is saved successfully";
+		model.addAttribute("employee", emp);
+		model.addAttribute("message",msg);
+		return "home";
 	}
 	
 	@PostMapping("/")
